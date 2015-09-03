1dd3fb2d668b13e2b11f47db13463819b166b6e8
Enio Costa <enio.m.costa@gmail.com>
2015-09-03 13:12:10 -0300

    Done getting adress.

--- crawl/crawl_adress.py
+++ crawl/crawl_adress.py
@@ -1,15 +1,37 @@
+# -*- coding: utf-8 -*-
+import urllib2
 from bs4 import BeautifulSoup
 
-class CrawlAdress(object):
+class CrawlAdress:
 
 	def __init__(self,cep):
-		crawl_url = ('http://www.buscacep.correios.com.br/servicos/dnec/consultaLogradouroAction.do?'+
+		self.crawl_url = ('http://www.buscacep.correios.com.br/servicos/dnec/consultaLogradouroAction.do?'+
 					'relaxation='+cep+'&'+
 					'Metodo=listaLogradouro&'+
 					'TipoConsulta=relaxation&'+
 					'StartRow=1&EndRow=10')
 
-	def getAdress(self):
-		soup = BeautifulSoup(self.crawl_url, 'html.parser')
+		self.html = self.getHTML()
 
-		print(soup.prettify())
+	def getAdress(self):
+		soup = BeautifulSoup(self.html, 'html.parser')
+		
+		trs = soup.find("table",{"bgcolor" : "gray"}).tr
+
+		if not trs:
+			print "Não foi encontrado nenhum endereço com o CEP informado"
+			return
+
+		else:
+			properties = ['Logradouro','Bairro','Localidade','UF','CEP']
+			cont = 0
+
+			for td in trs.find_all("td"):
+				print properties[cont]+": "+td.get_text()
+				cont+=1
+
+	def getHTML(self):
+		return urllib2.urlopen(self.crawl_url).read()
+
+	def showURL(self):
+		print self.crawl_url
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from crawl import crawl_adress
+from crawl.crawl_adress import CrawlAdress
 
 cep = raw_input("Informe o CEP: ").replace("-","")
 
 crawl = CrawlAdress(cep)
+
+crawl.getAdress()
 
 
